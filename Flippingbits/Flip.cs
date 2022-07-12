3bb4bb2e08a6a2a88a1166c9f89052ff6ed6585f
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-07-12 13:08:05 +0300

    long sayı 32 bit uzunluğunda ise solda boş  bitler için 0 koymuyoruz

---
+++ Flippingbits/Flip.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flippingbits
+{
+    public static class Flip
+    {
+        public static long FlipNumber(long n)
+        {
+            List<long> listOfInts = new List<long>();
+            List<long> listOfReverseInts = new List<long>();
+
+            while (n > 0)
+            {
+                listOfInts.Add(n % 2);
+                n /= 2;
+            }
+            listOfInts.Reverse();
+            List<long> predigits = new List<long>();
+            if (listOfInts.Count <32)
+            {
+                for (int i = 0; i < 32 - listOfInts.Count; i++)
+                {
+                    predigits.Add(0);
+                }
+                predigits.AddRange(listOfInts);
+            }
+            if (listOfInts.Count == 32)
+            {
+                predigits = listOfInts;
+
+            }
+            Console.WriteLine($"predigits.Count:{predigits.Count}");
+            predigits.ForEach(i =>
+            {
+                Console.Write($"{i}");
+            }
+            );
+
+            Console.WriteLine("");
+            Console.WriteLine($"1***********************************");
+            for (int i = 0; i < predigits.Count; i++)
+            {
+                if (predigits[i] == 1)
+                {
+                    predigits[i] = 0;
+                }
+                else if (predigits[i] == 0)
+                {
+                    predigits[i] = 1;
+                }
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"2***********************************");
+            predigits.ForEach(i =>
+            {
+                Console.Write($"{i}");
+
+            }
+            );
+            Console.WriteLine("");
+            Console.WriteLine($"***********************************");
+            long deci = 0;
+            predigits.Reverse();
+            for (int i = 0; i < predigits.Count; i++)
+            {
+
+
+                if (predigits[i] == 1)
+                {
+                    //Console.Write($"{(long)(Math.Pow(2, i))}*");
+                    deci = deci + (long)(Math.Pow(2, i));
+                }
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"**deci:{deci}*********************************");
+            return deci;
+        }
+    }
+}
--- Flippingbits/Program.cs
+++ Flippingbits/Program.cs
@@ -1,69 +1,30 @@
-﻿long n=9;
-List<long> listOfInts = new List<long>();
-List<long> listOfReverseInts = new List<long>();
+﻿
+using Flippingbits;
 
-while (n>0)
-{
-    listOfInts.Add(n % 2);
-    n /= 2;
-}
-listOfInts.Reverse();
-List<long> predigits = new List<long>();
-if (listOfInts.Count < 32)
-{
-    for (int i = 0; i < 32- listOfInts.Count; i++)
-    {
-        predigits.Add(0);
-    }
-    predigits.AddRange(listOfInts);
+List<long> listOfInts = new List<long>{
+    //4,
+    //16384,
+    //16,
+    //262144,
+    //268435456,
+    //1024,
+    //1048576,
+    //16,
+    //16777216,
+    2147483648,
+    //1,
+    //4194304,
+    //16384,
+    //128
+};
 
-}
-Console.WriteLine($"predigits.Count:{predigits.Count}");
-predigits.ForEach(i =>
-{
-    Console.Write($"{i}");
-    
-}
-);
-Console.WriteLine("");
-Console.WriteLine($"1***********************************");
-for (int i = 0; i < predigits.Count; i++)
-{
-    if (predigits[i] == 1)
-    {
-        predigits[i]=0;
-    }
-    else if (predigits[i] == 0)
-    {
-        predigits[i]=1;
-    }
 
-}
-Console.WriteLine("");
-Console.WriteLine($"2***********************************");
-predigits.ForEach(i =>
+foreach (var number in listOfInts)
 {
-    Console.Write($"{i}");
-
+    var result = Flip.FlipNumber(number);
+    Console.WriteLine(result);
 }
-);
-Console.WriteLine("");
-Console.WriteLine($"***********************************");
-long deci = 0;
-predigits.Reverse();
-for (int i = 0; i < predigits.Count; i++)
-{
-    Console.WriteLine($"#{predigits[i]}#");
 
-    if (predigits[i] == 1)
-    {
-        Console.Write($"{(long)(Math.Pow(2, i))}*");
-        deci = deci + (long)(Math.Pow(2, i));
-    }
-
-}
-Console.WriteLine("");
-Console.WriteLine($"**deci:{deci}*********************************");
 
 
 
