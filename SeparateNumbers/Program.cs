d24df82b670b3eb4ba615e4a3d7e3cf4191db3e5
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-10-21 03:21:53 +0300

    index error

---
+++ SeparateNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//string s = "7";
+//string s ="1234";
+//string s = "91011";
+string s = "99100";
+//string s = "101103";
+//string s ="010203";
+//string s = "13";
+//string s ="1";
+
+
+for (int i = 1; i< s.Length; i++)
+{
+    for (int j = 1; j < s.Length; j++)
+    {
+        if (i + j < s.Length)
+        {
+           // var firstnumber = Convert.ToInt64(s.Substring(0, i));
+
+            var secondnumber = Convert.ToInt64(s.Substring(i, j));
+
+            Console.WriteLine($" {i} {i+j}");
+            
+            //Console.WriteLine($"{secondnumber}-{firstnumber}  => {secondnumber - firstnumber} ");
+        }
+
+
+
+    }
+    
+}
+
+
+
